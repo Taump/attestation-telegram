ccdfd97e0215483feda36b947935d471d5e63b3d
Taump <taump@yandex.ru>
2024-12-11 23:55:09 +0400

    feat: add dictionary

---
+++ dictionary/telegram.local.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ATTESTATION_COMMAND: 'To get started, please use the /attest command.',
+    SEND_WALLET: 'Please send your own wallet address; it will be verified.',
+    COMMAND_ATTESTATION_AGAIN: 'To start the attestation process again, please use the /attest command.',
+    REMOVE_ADDRESS: 'You can remove your wallet address before verification by using the /remove command.',
+}
