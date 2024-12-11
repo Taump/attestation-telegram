ee22c92e58617eb26a11d7c0d93c195619354640
Taump <taump@yandex.ru>
2024-12-11 23:57:40 +0400

    feat: add JSDoc comments for TelegramMessageEmitter events

--- src/TelegramMessageEmitter.js
+++ src/TelegramMessageEmitter.js
@@ -1,3 +1,14 @@
+/**
+ * Event emitter for Telegram-related events
+ * @typedef {import('node:events').EventEmitter}
+ * 
+ * @event message - Emitted when a new message is received
+ * @type {object} payload - Message details
+ * 
+ * @event error - Emitted when an error occurs
+ * @type {Error} error - Error details
+ */
+
 const EventEmitter = require('events');
 
 const eventEmitter = new EventEmitter();
